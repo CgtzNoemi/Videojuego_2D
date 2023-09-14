5d43d6fe79e5cfca0e1ecec471116e53ee3ea94e
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2023-09-13 20:56:41 -0600

    3.2. Los prefabs

--- Videojuego2D/Assets/Scripts/OwletMovement.cs
+++ Videojuego2D/Assets/Scripts/OwletMovement.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
-public class JohnMovement : MonoBehaviour
+public class OwletMovement : MonoBehaviour
 {
+    public GameObject bulletPrefab;
     public float JumpForce;
     public float Speed;
 
@@ -11,6 +13,7 @@ public class JohnMovement : MonoBehaviour
     private Animator Animator;
     private float Horizontal;
     private bool EnElSuelo;
+    private float UltimoDisparo;
     void Start()
     {
         Rigidbody2D = GetComponent<Rigidbody2D>();
@@ -23,21 +26,23 @@ public class JohnMovement : MonoBehaviour
         if (Horizontal < 0.0f)
         {
             transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
-        }else if (Horizontal > 0.0f)
+        }
+        else if (Horizontal > 0.0f)
         {
             transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
         }
 
         Animator.SetBool("running", Horizontal != 0.0f);
         Animator.SetBool("jumping", Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow));
+        
 
 
-        Debug.DrawRay(transform.position, Vector3.down * 0.1f, Color.red);
-        if(Physics2D.Raycast(transform.position, Vector3.down, 0.1f))
+        Debug.DrawRay(transform.position, Vector3.down * 0.2f, Color.red);
+        if (Physics2D.Raycast(transform.position, Vector3.down, 0.2f))
         {
             EnElSuelo = true;
         }
-        else 
+        else
             EnElSuelo = false;
 
 
@@ -45,14 +50,43 @@ public class JohnMovement : MonoBehaviour
         {
             Jump();
         }
+        if (Input.GetKeyDown(KeyCode.Space) && Time.time > UltimoDisparo + 0.25f)
+        {
+            Shoot();
+            UltimoDisparo = Time.time;
+            Animator.SetBool("attacking", true);
+        }
+        else
+        {
+            Animator.SetBool("attacking", false);
+        }
     }
     private void Jump()
     {
         Rigidbody2D.AddForce(Vector2.up * JumpForce);
     }
+    private void Shoot()
+    {
+        Vector3 direction;
+        if (transform.localScale.x == 1.0f)
+        {
+            direction = Vector3.right;
+        }
+        else
+        {
+            direction = Vector3.left;
+        }
 
+        GameObject bullet = Instantiate(bulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        Vector3 newPosition = bullet.transform.position;
+        newPosition.y -= 0.1f; 
+        
+        bullet.transform.position = newPosition;
+        bullet.GetComponent<bulletScript>().SetDirection(direction);
+    }
     private void FixedUpdate()
     {
         Rigidbody2D.velocity = new Vector2(Horizontal, Rigidbody2D.velocity.y);
     }
 }
+
