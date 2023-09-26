a7d75bd675480a86dc4bbbce4672b7c4d0b1e2ac
CgtzNoemi <89617943+CgtzNoemi@users.noreply.github.com>
2023-09-26 17:49:32 -0600

    3.6. El movimiento de una bala

---
+++ Videojuego2D/Assets/Scripts/PinkMonsterScript.cs
@@ -0,0 +1,59 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PinkMonsterScript : MonoBehaviour
+{
+    public GameObject bulletPrefab; 
+    public GameObject Owlet;
+    private Animator Animator;
+
+    private float LastShoot;
+
+    void Start()
+    {
+        Animator = GetComponent<Animator>();
+    }
+
+    private void Update()
+    {
+        Vector3 direction = Owlet.transform.position - transform.position;
+        if (direction.x >= 0.0f) transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        else transform.localScale = new Vector3(-1.0f, 1.0f, 1.0f);
+
+        float distance = Mathf.Abs(Owlet.transform.position.x - transform.position.x);
+
+        if(distance < 1.0f && Time.time > LastShoot + 0.25f)
+        {
+            Shoot();
+            LastShoot = Time.time;
+            Animator.SetBool("attacking", true);
+        }
+        else
+        {
+            Animator.SetBool("attacking", false);
+        }
+
+    }
+
+    private void Shoot()
+    {
+        Debug.Log("Disparo");
+        Vector3 direction;
+        if (transform.localScale.x == 1.0f)
+        {
+            direction = Vector3.right;
+        }
+        else
+        {    
+            direction = Vector3.left;
+        }
+
+        GameObject bullet = Instantiate(bulletPrefab, transform.position + direction * 0.1f, Quaternion.identity);
+        Vector3 newPosition = bullet.transform.position;
+        newPosition.y -= 0.1f;
+
+        bullet.transform.position = newPosition;
+        bullet.GetComponent<bulletScript>().SetDirection(direction);
+    }
+}
